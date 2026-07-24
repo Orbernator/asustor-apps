201aa09d080ecb756f76fadb8ee73f109d174bb1
Orbernator <github@teamorbi.net>
2026-07-24 14:27:56 +0930

    Glance 0.8.5-o1
    Fixed a issue with Glance uninstalling

--- glance-adm/CONTROL/pre-uninstall.sh
+++ glance-adm/CONTROL/pre-uninstall.sh
@@ -2,20 +2,22 @@
 
 echo "glance-adm: --== pre-uninstall ==--"
 
+
 # Environment variables
 GLANCE_VERSION=$(cat $APKG_PKG_DIR/glance_version)
-GLANCE_CONTAINER=$(docker container ls -a | grep -E '^glance\b' | awk '{print $1}')
+GLANCE_CONTAINER=$(docker container ls -a | grep glance | awk '{print $1}')
 GLANCE_IMAGE=$(docker images | grep glanceapp/glance | grep $GLANCE_VERSION | awk '{print $3}')
 
-# Force shutdown of the containers and delete them
-echo "glance-adm: Stopping and removing containers"
-for cid in $GLANCE_CONTAINER; do
-  echo "    - $cid"
-  docker kill "$cid" 2>/dev/null
-  docker rm -f "$cid" 2>/dev/null
-done
+# Force shutdown of the container and delete it
+echo "glance-adm: Stopping and removing container"
+echo "    - $GLANCE_CONTAINER"
+if [ -n "$GLANCE_CONTAINER" ]; then
+  docker kill "$GLANCE_CONTAINER"
+  sleep 2
+  docker rm -f "$GLANCE_CONTAINER"
+fi
 
-# Remove docker image on uninstalling & updating
+# REMOVE docker image on uninstalling & updating
 echo "glance-adm: Removing docker image"
 echo "glance-adm: Image ID: $GLANCE_IMAGE"
 if [ -n "$GLANCE_IMAGE" ]; then
